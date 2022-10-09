23953ed28e9cd7db118a042cb33c1cb40189b0e3
Anthony Capirchio <acapirchio@outlook.com>
2022-10-09 21:53:57 +0200

    Updating about

--- content/pages/a-propos.md
+++ content/pages/a-propos.md
@@ -30,11 +30,12 @@ seo:
 layout: page
 ---
 
-__Mon but est clair: débusquer les problèmes et les éradiquer !__
+Je cherche les problèmes mais dans un seul et même objectif : les résoudre de la manière la plus cohérente possible. 
 
-Concrètement, je vous accompagne dans vos projets web qu’il s’agisse d’un MVP*, de l'industrialisation, l'évolution ou encore la refonte de votre solution.
+Cohérente car il existe généralement plusieurs solutions à un problème, encore faut-il bien appréhender les différentes contraintes, qu’elles soient techniques, métier ou même législatives, pour déterminer la plus à même de répondre au besoin. 
 
+Depuis plus de dix ans j’interviens dans différents contextes, pour du développement (principalement JS et Go), de la conception (produit, architecture) ainsi que la gestion de projets techniques. 
 
+Aujourd’hui j’interviens comme Freelance pour accompagner dans le cadrage et l’exécution, aussi bien avec les équipes produit que techniques. 
 
-
-_*Most Valuable Product, le minimum pour attaquer un marché_
+Vous avez un projet ou besoin de renforcer vos équipes ? Je vous invite à prendre contact soit via LinkedIn, soit Malt. 
--- src/layouts/home.js
+++ src/layouts/home.js
@@ -9,7 +9,7 @@ import Icon from '../components/Icon';
 import { getPageUrl, Link, withPrefix } from '../utils';
 
 export default class Home extends React.Component {
-    renderPost(post, index, hasMoreLink, moreLinkText) {
+    renderPost(post, index) {
         const title = _.get(post, 'title');
         const thumbImage = _.get(post, 'thumb_img_path');
         const excerpt = _.get(post, 'excerpt');
@@ -46,23 +46,35 @@ export default class Home extends React.Component {
                         <p>{excerpt}</p>
                     </div>
                 )}
-                {hasMoreLink && moreLinkText && (
-                    <p className="read-more">
-                        <Link className="read-more-link" href={postUrl}>{moreLinkText} <span className="icon-arrow-right" aria-hidden="true" /></Link>
-                    </p>
-                )}
+                <p className="read-more">
+                    <Link className="read-more-link" href={postUrl}>{"Continuer la lecture"} <span className="icon-arrow-right" aria-hidden="true" /></Link>
+                </p>
             </article>
         );
     }
 
+    renderPostsFreed(posts) {
+        return <div className="post-feed">
+            {posts.map((post, index) => {
+                return this.renderPost(post, index);
+            })}
+        </div>
+    }
+    
+    renderPhotograpiesFreed(photographies) {
+        return <div className="photography-feed">
+            {photographies.map((photography, index) => {
+                return this.renderPost(photography, index)
+            })}
+        </div>
+    }
+
     render() {
-        const data = _.get(this.props, 'data');
-        const config = _.get(data, 'config');
-        const header = _.get(config, 'header');
-        const headerImage = _.get(header, 'background_img');
-        const page = _.get(this.props, 'page');
-        const hasMoreLink = _.get(page, 'has_more_link');
-        const moreLinkText = _.get(page, 'more_link_text');
+        const data = this.props.data;
+        const config = data.config;
+        const header = config.header;
+        const headerImage = header.background_img;
+        const page = this.props.page;
         const posts = _.orderBy(_.get(this.props, 'posts', []), 'date', 'desc');
         const photographies = _.orderBy(_.get(this.props, 'photographies', []), 'date', 'desc');
 
@@ -71,12 +83,8 @@ export default class Home extends React.Component {
                 <Header config={config} page={page} image={headerImage} />
                 <div id="content" className="site-content">
                     <main id="main" className="site-main inner">
-                        <div className="post-feed">
-                            {_.map(posts, (post, index) => this.renderPost(post, index, hasMoreLink, moreLinkText))}
-                        </div>
-                        <div className="photography-feed">
-                            {_.map(photographies, (photography, index) => this.renderPost(photography, index, hasMoreLink, moreLinkText))}
-                        </div>
+                        {this.renderPostsFreed(posts)}
+                        {this.renderPhotograpiesFreed(photographies)}
                     </main>
                     <Footer config={config} />
                 </div>
